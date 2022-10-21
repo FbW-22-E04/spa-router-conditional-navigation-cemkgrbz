fdb70852ef100b3259f81c803d4c11c80a14bb69
Cem <cemkgrbz@gmail.com>
2022-10-21 10:37:18 +0200

    added solution

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+
 
 export const Login = () => {
   const [data, setData] = useState({
@@ -6,16 +8,40 @@ export const Login = () => {
     password: "r3@ct",
   });
 
+
+  const navigate = useNavigate()
+
+  const handleLogin = (e) => {
+    setData({
+        ...data, 
+        [e.target.id]: e.target.value
+    });
+  }
+
+  const handleSubmit = (e) => {
+
+    e.preventDefault()
+
+    if (data.userName === "john doe" && data.password === "r3@ct") {
+        return navigate("/home")
+        } else {
+          alert('wrong username and password')
+        }
+    }
+
   return (
     <>
       <h1>Login</h1>
-      <p>The username or password is incorrect!</p>
+      
+         {(data.userName !== "john doe" || data.password !== "r3@ct") &&<p className="inc-alert">The username or password is incorrect!</p>}
+
+      
       <form>
-        <input type="text" name="userName" placeholder="Username" />
+        <input type="text" name="userName" placeholder="Username" id="userName" onChange={handleLogin}/>
 
-        <input type="password" name="password" placeholder="Password" />
+        <input type="password" name="password" placeholder="Password" id="password" onChange={handleLogin}/>
 
-        <input type="submit" value="Submit" />
+        <input type="submit" value="Submit" onClick={handleSubmit}/>
       </form>
     </>
   );
